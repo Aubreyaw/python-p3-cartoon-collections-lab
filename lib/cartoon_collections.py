83d0a0d2a952f003d0d1ac7335dc26db9e7a370d
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-28 17:15:52 -0400

    Completes lab.

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarf_names = []):
+    for index, item in enumerate(dwarf_names, start=1):
+        print(f"{index}. {item}")
     pass
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+     new_calls = [call.capitalize() + "!" for call in calls]
+     return new_calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    sorted_calls = any(len(call) > 4 for call in calls)
+    return sorted_calls
+    
 
-def find_the_cheese():
-    pass
+def find_the_cheese(items):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in items:
+        if item in cheeses:
+            return item
+    return None
+    
+
